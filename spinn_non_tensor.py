c85873dceea921656807b002341c103caff0378d
such-a-git <adamperrett3142@gmail.com>
2019-06-20 18:57:07 +0100

    Can now pass in networks of neurons and parameters, beginnings of testing in xor, need to convert to no matrix format for allowing of multapses and incorporation of delays

--- spinn_non_tensor.py
+++ spinn_non_tensor.py
@@ -4,10 +4,11 @@ from copy import deepcopy
 
 class SpiNN_LIF_curr(object):
 
-    def __init__(self, weights, dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0, tau_refract=5.0, v_thresh=-50.0, v_reset=0.0, i_offset=0.0, alpha=0.05):
+    def __init__(self, weights, exc_or_inb='excitatory', dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0, tau_syn_E=5.0, tau_syn_I=5.0, tau_refract=5.0, v_thresh=-50.0, v_reset=0.0, i_offset=0.0, alpha=0.05):
         self.alpha = alpha
-        # passed in varaibles of the neuron
         self.dt = dt
+        # passed in varaibles of the neuron
+        self.exc_or_inb = exc_or_inb
         self.v_rest = v_rest #* 10**-3
         self.cm = cm #* 10**-3
         self.tau_m = tau_m #* 10**-3
@@ -16,6 +17,10 @@ class SpiNN_LIF_curr(object):
         self.v_thresh = v_thresh #* 10**-3
         self.v_reset = v_reset #* 10**-3
         self.i_offset = i_offset
+        self.curr_init_E = np.exp(-dt / tau_syn_E)
+        self.curr_decay_E = (tau_syn_E / dt) * (1.0 - np.exp(-dt / tau_syn_E))
+        self.curr_init_I = np.exp(-dt / tau_syn_I)
+        self.curr_decay_I = (tau_syn_I / dt) * (1.0 - np.exp(-dt / tau_syn_I))
         # state variables
         self.v = self.v_rest
         self.scaled_v = (self.v - self.v_thresh) / self.v_thresh
@@ -25,10 +30,6 @@ class SpiNN_LIF_curr(object):
         self.weights = weights
         self.has_spiked = False
         self.received_spikes = [False for i in range(len(weights))]
-
-    # activation function
-    def H(self, x):
-        return 1 / (1 + np.exp(-x))
 
     # operation to be performed when not spiking
     def integrating(self):
@@ -51,10 +52,16 @@ class SpiNN_LIF_curr(object):
 
     # collect current input from spikes
     def sum_the_spikes(self):
-        self.i_in *= 0.8187307530779818
+        if self.exc_or_inb == 'excitatory':
+            self.i_in *= self.curr_decay_E
+        else:
+            self.i_in *= self.curr_decay_I
         for neuron in range(len(self.received_spikes)):
             if self.received_spikes[neuron]:
-                self.i_in += self.weights[neuron] * 0.9063462346100909
+                if self.weights[neuron] > 0:
+                    self.i_in += self.weights[neuron] * self.curr_init_E
+                else:
+                    self.i_in += self.weights[neuron] * self.curr_init_I
         return self.i_in
 
     # calculates the current for all inputs
@@ -77,11 +84,74 @@ class SpiNN_LIF_curr(object):
         dEdVt1 = 0
         dEdVt = (dEdz * dzdu / network.v_thresh) + (dEdVt1 * self.alpha)
         dEdzt = dEdz - (dEdVt1 * dt * self.v_thresh) + (dEdVt1 * self.weights["blah"] * (1 - self.alpha) * self.r_mem)
-
+        
+class xor_neuron(object):
+    
+    def __init__(self, rate, stochastic):
+        self.rate = rate
+        self.dt = 0
+        self.has_spiked = False
+        self.stochastic = stochastic
+        self.received_spikes = []
+        
+    def step(self):
+        if self.stochastic:
+            if np.random.random() < float(self.dt) / float(self.rate):
+                self.has_spiked = True
+            else:
+                self.has_spiked = False
+        else:
+            if self.dt % self.rate == 0:
+                self.has_spiked = True
+            else:
+                self.has_spiked = False
+        self.dt += 1
+        
+        
 class Network(object):
 
-    def __init__(self, weight_matrix, dt=1.0, v_rest=0.0, cm=1.0, tau_m=20.0, tau_refract=5.0, v_thresh=1.0, v_reset=0.0, i_offset=0.0):
+    def __init__(self, weight_matrix, e_size=0, i_size=0, dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0, tau_syn_E=5.0, tau_syn_I=5.0, tau_refract=5.0, v_thresh=-55.0, v_reset=0.0, i_offset=0.0):
         self.alpha = dt / tau_m
+        # neuron variable
+        self.dt = dt
+        self.v_rest = v_rest #* 10**-3
+        self.cm = cm #* 10**-3
+        self.tau_m = tau_m #* 10**-3
+        self.r_mem = self.tau_m / cm
+        self.tau_refract = tau_refract
+        self.v_thresh = v_thresh #* 10**-3
+        self.v_reset = v_reset #* 10**-3
+        self.i_offset = i_offset
+        # network variables
+        self.e_size = e_size
+        self.i_size = i_size
+        self.weight_matrix = weight_matrix
+        self.number_of_neurons = len(weight_matrix)
+        self.neuron_list = []
+        self.did_they_spike = [False for i in range(self.number_of_neurons)]
+
+        # initialise the network of neurons connected
+        for neuron in range(self.i_size):
+            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron], 'inhibitory', dt, v_rest, cm, tau_m, tau_syn_E, tau_syn_I, tau_refract, v_thresh, v_reset, i_offset, self.alpha))
+        for neuron in range(self.i_size, self.number_of_neurons):
+            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron], 'excitatory', dt, v_rest, cm, tau_m, tau_syn_E, tau_syn_I, tau_refract, v_thresh, v_reset, i_offset, self.alpha))
+
+    # step all neurons and save state
+    def step(self):
+        spike_tracking = []
+        for neuron in self.neuron_list:
+            neuron.received_spikes = self.did_they_spike
+            neuron.step()
+            spike_tracking.append(neuron.has_spiked)
+        self.did_they_spike = spike_tracking
+
+class Network_in_xor(object):
+
+    def __init__(self, weight_matrix, delay_matrix, on_rate, off_rate, score_delay, xor_input, stochastic=False, excite_params={}, inhib_params={},
+                 dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0,  tau_syn_E=5.0, tau_syn_I=5.0, tau_refract=5.0, v_thresh=-50.0, v_reset=-65.0, i_offset=0.0):
+        self.alpha = dt / tau_m
+        self.excite_params = excite_params
+        self.inhib_params = inhib_params
         # neuron variable
         self.dt = dt
         self.v_rest = v_rest #* 10**-3
@@ -96,70 +166,90 @@ class Network(object):
         self.weight_matrix = weight_matrix
         self.number_of_neurons = len(weight_matrix)
         self.neuron_list = []
-        self.did_it_spike = [False for i in range(self.number_of_neurons)]
+        self.did_they_spike = [False for i in range(self.number_of_neurons)]
+        # environment variable
+        self.inputs = 2
+        self.outputs = 2
+        self.on_rate = on_rate
+        self.off_rate = off_rate
+        self.stochastic = stochastic
+        self.score_delay = score_delay
+        self.score_tracking = []
+        self.output_spike_count = [0 for i in range(self.outputs)]
+        if xor_input[0] == xor_input[1]:
+            self.xor_output = 0
+        else:
+            self.xor_output = 1
 
         # initialise the network of neurons connected
-        for neuron in range(self.number_of_neurons):
-            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron], dt, v_rest, cm, tau_m, tau_refract, v_thresh, v_reset, i_offset, self.alpha))
+        for env_neuron in range(self.inputs):
+            if xor_input[env_neuron]:
+                self.neuron_list.append(xor_neuron(on_rate, stochastic))
+            else:
+                self.neuron_list.append(xor_neuron(off_rate, stochastic))
+        for neuron in range(self.inputs, len(self.excite_params) + self.inputs):
+            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron],
+                                                   'excitatory',
+                                                   excite_params["dt"],
+                                                   excite_params["v_rest"],
+                                                   excite_params["cm"],
+                                                   excite_params["tau_m"],
+                                                   excite_params["tau_syn_E"],
+                                                   excite_params["tau_syn_I"],
+                                                   excite_params["tau_refract"],
+                                                   excite_params["v_thresh"],
+                                                   excite_params["v_reset"],
+                                                   excite_params["i_offset"],
+                                                   self.alpha))
+        for neuron in range(len(self.excite_params) + self.inputs, len(self.inhib_params) + len(self.excite_params) + self.inputs):
+            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron],
+                                                   'excitatory',
+                                                   inhib_params["dt"],
+                                                   inhib_params["v_rest"],
+                                                   inhib_params["cm"],
+                                                   inhib_params["tau_m"],
+                                                   inhib_params["tau_syn_E"],
+                                                   inhib_params["tau_syn_I"],
+                                                   inhib_params["tau_refract"],
+                                                   inhib_params["v_thresh"],
+                                                   inhib_params["v_reset"],
+                                                   inhib_params["i_offset"],
+                                                   self.alpha))
+        for neuron in range(len(self.inhib_params) + len(self.excite_params) + self.inputs, self.number_of_neurons):
+            self.neuron_list.append(SpiNN_LIF_curr(self.weight_matrix[neuron], 'excitatory', dt, v_rest, cm, tau_m,
+                                                   tau_syn_E, tau_syn_I, tau_refract, v_thresh, v_reset, i_offset,
+                                                   self.alpha))
+
+    def add_delays(self):
+        # need to get rid of connection matrix as that fucks multapses
 
     # step all neurons and save state
-    def step(self):
+    def step(self, current_dt):
         spike_tracking = []
         for neuron in self.neuron_list:
-            neuron.received_spikes = self.did_it_spike
+            neuron.received_spikes = self.did_they_spike
             neuron.step()
             spike_tracking.append(neuron.has_spiked)
-        self.did_it_spike = spike_tracking
+        self.output_spike_count[0] += self.did_they_spike[number_of_neurons-2]
+        self.output_spike_count[1] += self.did_they_spike[number_of_neurons-1]
+        self.score(current_dt)
+        self.did_they_spike = spike_tracking
 
-def calc_error(spike_history, single_error_neuron=False):
-    target_hz = 10.0
-    if single_error_neuron:
-        actual_hz = float(sum(spike_history[number_of_neurons-1])) / float(T)
-    else:
-        actual_hz = float(sum([sum(spike_history[n]) for n in range(number_of_neurons)])) / float(T)
-    error = 0.5 * (target_hz - actual_hz)**2
-    return error
+    def score(self, current_dt):
+        if current_dt % self.score_delay == 0 and current_dt:
+            if self.output_spike_count[0] > self.output_spike_count[1] and self.xor_output:
+                score = 0.0
+            elif self.output_spike_count[0] < self.output_spike_count[1] and self.xor_output:
+                score = 1.0
+            elif self.output_spike_count[0] > self.output_spike_count[1] and not self.xor_output:
+                score = 1.0
+            elif self.output_spike_count[0] < self.output_spike_count[1] and not self.xor_output:
+                score = 0.0
+            else:
+                score = 0.0
+            self.score_tracking.append(score)
+            self.output_spike_count = [0 for i in range(self.outputs)]
 
-# error = 0.5 (y - y_target)^2
-def back_prop(spike_history, voltage_history, network):
-    error = calc_error(spike_history)
-    print "Error for the last iteration is", error
-    new_weight_matrix = deepcopy(network.weight_matrix)
-    # theta = 1
-    # dEdz = theta * error
-    # dzdu = 0
-    # dEdVt1 = 0
-    # dEdVt = (dEdz * dzdu / network.v_thresh) + (dEdVt1 * network.alpha)
-    # dEdzt = dEdz - (dEdVt1 * dt * network.v_thresh) + \
-    #         sum([dEdVt1 * network.weight_matrix["blah"] * (1 - network.alpha) * network.r_mem for neuron in network.neuron_list])
-    dEdz = [[0.0 for i in range(T/dt + 1)] for neuron in range(number_of_neurons)]
-    dEdV = [[0.0 for i in range(T/dt + 1)] for neuron in range(number_of_neurons)]
-    # dEdWi = [[[0.0 for i in range(T/dt + 1)] for pre in range(number_of_neurons)] for post in range(number_of_neurons)]
-    # dEdWr = [[[0.0 for i in range(T/dt + 1)] for pre in range(number_of_neurons)] for post in range(number_of_neurons)]
-    dEdWi = [[0.0 for pre in range(number_of_neurons)] for post in range(number_of_neurons)]
-    dEdWr = [[0.0 for pre in range(number_of_neurons)] for post in range(number_of_neurons)]
-    for t in range(T/dt-1, -1, -1):
-        for neuron in range(number_of_neurons):
-            this_neuron = network.neuron_list[neuron]
-            if spike_history[neuron][t]:
-                p_dEdz = error
-                sum_dEdV = sum([dEdV[n][t+1] *
-                                weight_matrix[neuron][n] *
-                                (1-network.neuron_list[n].alpha) *
-                                this_neuron.r_mem
-                                for n in range(number_of_neurons)])
-                dEdz[neuron][t] = p_dEdz - (dEdV[neuron][t+1] * dt * this_neuron.v_thresh) + sum_dEdV
-                dEdV[neuron][t] = dEdz[neuron][t] * ((1/dt)/voltage_history[neuron][t]) * (1/this_neuron.v_thresh) + \
-                                  (dEdV[neuron][t+1] * this_neuron.alpha)
-
-    for pre in range(number_of_neurons):
-        for post in range(number_of_neurons):
-            dEdWi[pre][post] = sum([dEdV[post][t] * spike_history[pre][t] for t in range(T/dt)])
-            dEdWr[pre][post] = sum([dEdV[post][t] * spike_history[pre][t] for t in range(T/dt)])
-
-            new_weight_matrix[pre][post] += l_rate * dEdWi[pre][post]
-
-    return new_weight_matrix
 
 # Network
 max_weight = 0.001
@@ -167,7 +257,7 @@ number_of_neurons = 6
 weight_matrix = [[np.random.random() * max_weight for i in range(number_of_neurons)] for j in range(number_of_neurons)]
 # weight_matrix = []
 
-epochs = 100
+epochs = 0
 l_rate = 1
 # Duration of the simulation in ms
 T = 200
@@ -182,15 +272,14 @@ if weight_matrix:
 
     for epoch in range(epochs):
 
-        network = Network(weight_matrix=weight_matrix, tau_refract=0)
+        network = Network(weight_matrix=weight_matrix, tau_refract=0, dt=dt)
         spikes = [False for neuron in range(number_of_neurons)]
-        all_spikes = []
         # Output variables
         I = []
         V = []
-        scaled_V = []
         spike_history_index = []
         spike_history_time = []
+        all_spikes = []
 
         for step in range(steps):
 
@@ -198,23 +287,20 @@ if weight_matrix:
 
             t = step * dt
 
-            network.did_it_spike = spikes
+            network.did_they_spike = spikes
             network.step()
 
             # Set input current in mA and save var
             i = []
             v = []
-            sc = []
             spikes = []
             for neuron in network.neuron_list:
                 neuron.i_offset = np.random.random() * 1
                 i.append(neuron.i_offset)
                 v.append(neuron.v)
-                sc.append(neuron.scaled_v)
                 spikes.append(neuron.has_spiked)
             I.append(i)
             V.append(v)
-            scaled_V.append(sc)
 
             for neuron in range(number_of_neurons):
                 if spikes[neuron]:
@@ -223,10 +309,7 @@ if weight_matrix:
 
         I = np.transpose(I).tolist()
         V = np.transpose(V).tolist()
-        scaled_V = np.transpose(scaled_V).tolist()
         all_spikes = np.transpose(all_spikes).tolist()
-
-        weight_matrix = back_prop(all_spikes, scaled_V, network)
 
         if plot or epoch == 0 or epoch == epochs-1:
             plt.rcParams["figure.figsize"] = (12, 6)
