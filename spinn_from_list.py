60d314b71d9377fb2f682749f5e37bfc0b2935c5
such-a-git <adam.perrett@manchester.ac.uk>
2019-06-24 01:07:50 +0100

    delay added to spikes summation

--- spinn_from_list.py
+++ spinn_from_list.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 class SpiNN_LIF_curr(object):
 
-    def __init__(self, weights, exc_or_inb='excitatory', dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0, tau_syn_E=5.0,
+    def __init__(self, connections, exc_or_inb='excitatory', dt=1.0, v_rest=-65.0, cm=1.0, tau_m=20.0, tau_syn_E=5.0,
                  tau_syn_I=5.0, tau_refract=5.0, v_thresh=-50.0, v_reset=0.0, i_offset=0.0, max_delay=127):
         self.dt = dt
         # passed in varaibles of the neuron
@@ -27,9 +27,10 @@ class SpiNN_LIF_curr(object):
         self.t_rest = 0.0
         self.i_in = self.i_offset
         # network variables
-        self.weights = weights
+        self.connections = connections
         self.has_spiked = False
-        self.received_spikes = [False for i in range(len(weights))]
+        self.received_spikes = [False for i in range(self.connections[0])]
+        del self.connections[0]
         self.spike_train = []
 
     # operation to be performed when not spiking
@@ -51,18 +52,32 @@ class SpiNN_LIF_curr(object):
         self.has_spiked = False
         self.t_rest -= 1.0
 
+    def extract_the_spikes(self):
+        for conn in self.connections:
+            if self.received_spikes[conn[0]]:
+                if conn[1] > 0:
+                    update = conn[1] * self.curr_init_E
+                else:
+                    update += conn[2] * self.curr_init_I
+                curr_param = {'update': update, 'delay': conn[2]}
+                self.spike_train.append(curr_param)
+
     # collect current input from spikes
     def sum_the_spikes(self):
         if self.exc_or_inb == 'excitatory':
             self.i_in *= self.curr_decay_E
         else:
             self.i_in *= self.curr_decay_I
-        for neuron in range(len(self.received_spikes)):
-            if self.received_spikes[neuron]:
-                if self.weights[neuron] > 0:
-                    self.i_in += self.weights[neuron] * self.curr_init_E
-                else:
-                    self.i_in += self.weights[neuron] * self.curr_init_I
+        self.extract_the_spikes()
+        to_be_deleted = []
+        for spike in self.spike_train:
+            if spike['delay'] <= 0:
+                self.i_in += spike['update']
+                to_be_deleted.append(spike)
+            else:
+                spike['delay'] -= self.dt
+        for deletable in to_be_deleted:
+            del self.spike_train[self.spike_train.index(deletable)]
         return self.i_in
 
     # calculates the current for all inputs
@@ -104,10 +119,14 @@ class Network(object):
         self.number_of_neurons = self.e_size + self.i_size
         self.neuron_list = []
         self.did_they_spike = [False for i in range(self.number_of_neurons)]
-        self.split_list = [[] for i in range(self.number_of_neurons)]
+        self.split_list = [[self.number_of_neurons] for i in range(self.number_of_neurons)]
 
+        # split and reverse conn list so only list of neurons received from is left
         for conn in conn_list:
-            self.split_list[conn[0]].append(conn[1:])
+            reverse_conn = deepcopy(conn)
+            reverse_conn[0] = reverse_conn[1]
+            reverse_conn[1] = conn[0]
+            self.split_list[reverse_conn[0]].append(reverse_conn[1:])
 
         # initialise the network of neurons connected
         for neuron in range(len(self.inhib_params)):
